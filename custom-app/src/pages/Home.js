f9219f5f9ee7e686a8599113a360cce31bd5dde3
nancytao <nancy.tao42@gmail.com>
2018-10-31 13:23:22 -0400

    Roll own authentication starter app

---
+++ custom-app/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@material-ui/core'
+
+const Home = () => (
+  <Typography variant="display1">Welcome!</Typography>
+);
+
+export default Home;
