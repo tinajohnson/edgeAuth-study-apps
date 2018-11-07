15f95a30c8b72d4b8cbadd2c9d678781973d3802
nancytao <nancy.tao42@gmail.com>
2018-11-06 21:59:50 -0500

    Remove serviceWorker.js

--- auth0-app/src/index.js
+++ auth0-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import auth0 from 'auth0-js';
 
 ReactDOM.render(
@@ -11,8 +10,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
 if (module.hot) module.hot.accept();
